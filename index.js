9125bd26a7b50290d25705a425fc4efc182e3ac9
Carlo Costantino <costantinoart@gmail.com>
2019-10-04 13:56:27 -1000

    Add functions that fetch and render results
    Added formatPerams() and added News API request to requestNews() as well as error handeling.
    Stored reply in a JSON string so I can add to it later. (for "search history" section)
    Rendered content w/renderResults() using resultsHTML() to build a results array.

--- index.js
+++ index.js
@@ -1,32 +1,73 @@
 'use strict';
 
-const DATA = ['test string', 'test value']
+let DATA = ''
+const baseURL = 'https://newsapi.org/v2/'
+const apiKey = '7ea5333e8e7c4da08923033bc7146c77'
 
 function handleFormSubmit() { 
   $('.js-searchbar').submit(event => {
     event.preventDefault();
-    const input = $('.js-search-input').val().toLowerCase();
-
-    createNews(input);
-    renderResults(DATA);
+    const userInput = $('.js-search-input').val().toLowerCase();
+    requestNews(baseURL, userInput);;
   })
 }
 
-function createNews(input) {
-  requestNews(createURL(input));
-  console.log("requestNews ran & the value is '" + input + "'");
+function requestNews(url, query) {
+  const search = 'everything?'
+  const perams = {
+    q: query,
+    language: 'en',
+    pageSize: 2,
+  };
+  const options = {
+    headers: new Headers({
+      'X-Api-Key': apiKey
+    })
+  };
+  const queryString = formatPerams(perams)
+  const fullURL = url + search + queryString;
+  console.log(fullURL);
+  fetch(fullURL, options)
+    .then(responce => {
+      if (responce.ok) {
+        return responce.json();
+      } else {
+        throw new Error();
+      }
+    })
+    .then(responceJson => {
+      if (responceJson.totalResults === 0) {
+        console.log("there are 0 results, search something else")
+      } else { 
+        DATA = JSON.stringify(responceJson);
+        renderResults(DATA);
+      }
+    })
+    .catch(err => {
+      alert("something bad happened :(");
+    })
 }
 
-function createURL(input) {
-  return input + "url-string"
-}
-
-function requestNews(url) {
-  console.log(url);
+function formatPerams(perams) {
+  const queryString = Object.keys(perams)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(perams[key])}`)
+    .join('&');
+    return queryString;
 }
 
 function renderResults(DATA) {
-  console.log("renderResults ran and the result data is '" + DATA + "'");
+  const dataObj = JSON.parse(DATA);
+  $('.js-results-list').empty()
+    .html(resultsHTML(dataObj));
+}
+
+function resultsHTML(dataObj) {
+  const listItems = []
+  const data = dataObj.articles
+  for (let i = 0; i < dataObj.articles.length; i++) {
+  listItems.push(`<li><img src="${data[i].urlToImage}" alt="article preview image" width="300px"><h3>${data[i].title}</h3><p>${data[i].description}</p></li>`)
+  }
+  return listItems;
 }
 
 function handleEntitySelect() {
@@ -36,6 +77,7 @@ function handleSortHistory() {
 }
 
 function startApp() {
+  // renderTopStories();
   handleFormSubmit();
   handleEntitySelect();
   handleSortHistory();
