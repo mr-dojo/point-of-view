3218a3b19e43d432fcab97b29585578ab0c14b1e
Carlo Costantino <costantinoart@gmail.com>
2019-10-04 10:41:24 -1000

    Added high level functions and global variables

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 'use strict';
 
-function handleFormSubmit() {
+const DATA = ['test string', 'test value']
+
+function handleFormSubmit() { 
+  $('.js-searchbar').submit(event => {
+    event.preventDefault();
+    const input = $('.js-search-input').val().toLowerCase();
+
+    createNews(input);
+    renderResults(DATA);
+  })
+}
+
+function createNews(input) {
+  requestNews(createURL(input));
+  console.log("requestNews ran & the value is '" + input + "'");
+}
+
+function createURL(input) {
+  return input + "url-string"
+}
+
+function requestNews(url) {
+  console.log(url);
+}
+
+function renderResults(DATA) {
+  console.log("renderResults ran and the result data is '" + DATA + "'");
 }
 
 function handleEntitySelect() {
